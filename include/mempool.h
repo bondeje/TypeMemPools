54bc159b8d9c8ebc878f6175a5ba6399100fae57
bondejeJBWin <jeffrey.bonde@gmail.com>
2024-07-30 11:24:08 -0700

    include guard

--- include/mempool.h
+++ include/mempool.h
@@ -1,3 +1,6 @@
+#ifndef MEMPOOL_H
+#define MEMPOOL_H
+
 #include <stddef.h>
 
 typedef struct MemPool MemPool;
@@ -13,4 +16,7 @@ void MemPoolManager_del(MemPoolManager * self);
 void * MemPoolManager_next(MemPoolManager * self);
 void * MemPoolManager_malloc(MemPoolManager * self, size_t size);
 void * MemPoolManager_aligned_alloc(MemPoolManager * self, size_t size, size_t alignment);
+
+#endif
+
 
